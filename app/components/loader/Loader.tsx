cc5e9358433513c6446f3f1f7cab3066f5b66a25
QuiteCustard <56020602+QuiteCustard@users.noreply.github.com>
2024-06-07 20:52:45 +0100

    Unnest css

--- app/components/loader/Loader.tsx
+++ app/components/loader/Loader.tsx
@@ -1,6 +1,7 @@
 "use client";
 import "@/components/loader/loader.css";
 import { signal } from "@preact/signals-react";
+import Link from "next/link";
 
 import { useSignals } from "@preact/signals-react/runtime";
 
@@ -12,6 +13,7 @@ export default function Loading() {
   return (
     <div id="loading-cover" className={loaderActive.value === true ? 'open' : ''}>
       <p className="pokedex-title">Loading...</p>
+      <p>Still not loaded after a few seconds? Click <Link href={"/"}>here</Link></p>
     </div>
   )
 }
